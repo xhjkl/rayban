d66c5ffa699e8cc5b76033f4b9384a77f24040fc
Denis Makarenko <decelas@gmail.com>
2015-12-11 23:28:38 +0600

    Core Data base

---
+++ Rayban/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Rayban
+//
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
